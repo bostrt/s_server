27fab078d8ad08f767b594c3b8dfae54ad8d131d
Robert Bost <bostrt@gmail.com>
2018-07-18 11:59:34 -0400

    Startup enhancements

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM centos:latest
 
 RUN yum install -y openssl && yum clean all
-RUN openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -subj '/CN=localhost' -nodes
 
 COPY run.sh .
 EXPOSE 8080 
--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-openssl s_server -cert cert.pem -key key.pem -accept 8080 -www $@
+_cert_filename=${CERT_FILENAME:="cert.pem"}
+_key_filename=${KEY_FILENAME:="key.pem"}
+_cert="./${_cert_filename}"
+_key="./${_key_filename}"
+
+if [ -d "/certs" ]; then
+        # Use mounted certificates volume.
+        _cert="/certs/${_cert_filename}"
+        _key="/certs/${_key_filename}"
+else
+        openssl req -x509 -newkey rsa:4096 -keyout $_key -out $_cert -days 365 -subj '/CN=localhost' -nodes
+fi
+
+openssl s_server -cert $_cert -key $_key -accept 8080 -www $@
